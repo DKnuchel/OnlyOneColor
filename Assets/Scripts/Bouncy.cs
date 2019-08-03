0b18b28fbea7ccca599cf4ee0e830c3427e6b616
Renato <renato.farruggio@stud.unibas.ch>
2019-08-03 14:34:26 +0200

    Implemented MasterSkript

---
+++ Assets/Scripts/Bouncy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncy : MonoBehaviour
+{
+    bool shouldJump = false;
+
+    [SerializeField] GameObject masterController = null;
+
+    MasterScript masterScript;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        masterScript = masterController.GetComponent<MasterScript>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        if(shouldJump && masterScript.isBouncing())
+        {
+            Debug.Log("Bounce");
+            rb.AddForce(0, 10, 0, ForceMode.VelocityChange);
+        }
+        
+        shouldJump = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        shouldJump = true;
+        rb.velocity = Vector3.zero;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        shouldJump = true;
+        rb.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/MasterScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterScript : MonoBehaviour
+{
+    [SerializeField] bool bouncing = false;
+    [SerializeField] bool minimized = false;
+    [SerializeField] bool sticky = false;
+    [SerializeField] bool speedBoost = false;
+    [SerializeField] bool ghostsActivated = false;
+    [SerializeField] bool gravitationReversed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool isBouncing()
+    {
+        return bouncing;
+    }
+
+    public bool isMinimized()
+    {
+        return minimized;
+    }
+
+    public bool isSticky()
+    {
+        return sticky;
+    }
+
+    public bool isSpeedBoosted()
+    {
+        return speedBoost;
+    }
+
+    public bool isGhostActivated()
+    {
+        return ghostsActivated;
+    }
+
+    public bool isGravitationalReversed()
+    {
+        return gravitationReversed;
+    }
+}
