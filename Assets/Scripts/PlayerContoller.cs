e42078f73b430a6aa17b889978a7946f24ca39e9
Renato <renato.farruggio@stud.unibas.ch>
2019-08-03 16:53:53 +0200

    Controlls now work fully

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+    [SerializeField] GameObject masterController;
+
+    MasterScript masterScript;
+
     bool isCollided = false;
     Rigidbody rb;
+    int speedMultiplier = 3;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        masterScript = masterController.GetComponent<MasterScript>();
     }
     void FixedUpdate()
     {
         if(isCollided)
         {
             float multiplier = 1f / (1f + rb.velocity.magnitude * 2);
+            bool isSpeedBoosted = masterScript.isSpeedBoosted();
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                rb.AddForce(new Vector3(-10, 0, 0) * multiplier, ForceMode.VelocityChange);
+                if (isSpeedBoosted)
+                { 
+                    rb.AddForce(new Vector3(-10 * speedMultiplier, 0, 0) * multiplier, ForceMode.VelocityChange);
+                } else
+                {
+                    rb.AddForce(new Vector3(-10, 0, 0) * multiplier, ForceMode.VelocityChange);
+                }
             }
             else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                rb.AddForce(new Vector3(10, 0, 0) * multiplier, ForceMode.VelocityChange);
+                if (isSpeedBoosted)
+                {
+                    rb.AddForce(new Vector3(10 * speedMultiplier, 0, 0) * multiplier, ForceMode.VelocityChange);
+                } else
+                {
+                    rb.AddForce(new Vector3(10, 0, 0) * multiplier, ForceMode.VelocityChange);
+                }
             }
         } else
         {
