49ebdaeb7ed3f2126c89e304e512e46e53b98dd2
Renato <renato.farruggio@stud.unibas.ch>
2019-08-03 19:08:26 +0200

    MasterScript now choses color code accordingly to buttons being pressed.

--- Assets/Scripts/MasterScript.cs
+++ Assets/Scripts/MasterScript.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class MasterScript : MonoBehaviour
 {
-    [SerializeField] bool bouncing = false;
-    [SerializeField] bool minimized = false;
-    [SerializeField] bool sticky = false;
-    [SerializeField] bool speedBoost = false;
-    [SerializeField] bool ghostsActivated = false;
-    [SerializeField] bool gravitationReversed = false;
+    int currentColor = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,41 +15,54 @@ public class MasterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        currentColor = 0;
+        bool key1, key2, key3;
+        key1 = Input.GetKey(KeyCode.Alpha1);
+        key2 = Input.GetKey(KeyCode.Alpha2);
+        key3 = Input.GetKey(KeyCode.Alpha3);
+
+        if (key1 && key2 && key3)
+        {
+            currentColor = 0;
+        }
+        else if (key2 && key3)
+        {
+            currentColor = 4;
+        }
+        else if (key1 && key3)
+        {
+            currentColor = 5;
+        }
+        else if (key1 && key2)
+        {
+            currentColor = 6;
+        }
+        else if (key1)
+        {
+            currentColor = 1;
+        }
+        else if (key2)
+        {
+            currentColor = 2;
+        }
+        else if (key3)
+        {
+            currentColor = 3;
+        }
     }
 
+    /*
+     * 0 white  nothing
+     * 1 red    bouncy
+     * 2 blue   minimize
+     * 3 yellow sticky
+     * 4 green  ghost
+     * 5 orange gravity
+     * 6 purple faster player
+    */
     int getCurrentColor()
     {
-        return 0;
+        return currentColor;
     }
-
-    public bool isBouncing()
-    {
-        return bouncing;
-    }
-
-    public bool isMinimized()
-    {
-        return minimized;
-    }
-
-    public bool isSticky()
-    {
-        return sticky;
-    }
-
-    public bool isSpeedBoosted()
-    {
-        return speedBoost;
-    }
-
-    public bool isGhostActivated()
-    {
-        return ghostsActivated;
-    }
-
-    public bool isGravitationalReversed()
-    {
-        return gravitationReversed;
-    }
+   
 }
