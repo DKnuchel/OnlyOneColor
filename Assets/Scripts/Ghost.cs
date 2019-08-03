aad441a7b52f38fc82d104c512cb18a08de475ac
Renato <renato.farruggio@stud.unibas.ch>
2019-08-03 16:57:51 +0200

    Added ghost script and gravity script

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    Collider collider = null;
+    [SerializeField] GameObject masterController = null;
+
+    MasterScript masterScript;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        masterScript = masterController.GetComponent<MasterScript>();
+        collider = GetComponent<Collider>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (masterScript.isGhostActivated())
+        {
+            collider.enabled = false;
+            rb.constraints = RigidbodyConstraints.FreezePosition;
+        } else
+        {
+            collider.enabled = true;
+            rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+    [SerializeField] GameObject masterController = null;
+
+    MasterScript masterScript;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        masterScript = masterController.GetComponent<MasterScript>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (masterScript.isGravitationalReversed())
+        {
+            rb.AddForce(Physics.gravity * -2);
+
+        }
+    }
+}
