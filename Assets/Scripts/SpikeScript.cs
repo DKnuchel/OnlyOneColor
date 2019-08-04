774d0835189a7e88633ee48f7abd6783758e25ed
Renato <renato.farruggio@bluewin.ch>
2019-08-04 17:35:46 +0200

    Added functionality and texture to spikes. they now reload the scene

---
+++ Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        
+    }
+}
