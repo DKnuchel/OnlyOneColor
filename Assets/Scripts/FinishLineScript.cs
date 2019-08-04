e419ab430ff86cac79e184267952c5cf46fa9626
Renato <renato.farruggio@bluewin.ch>
2019-08-04 16:00:01 +0200

    Added FinishLine block and scene change

---
+++ Assets/Scripts/FinishLineScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLineScript : MonoBehaviour
+{
+
+    [SerializeField] string nextLevelName;
+    [SerializeField] GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //nextLevelName = "level1";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == player)
+        SceneManager.LoadScene(nextLevelName, LoadSceneMode.Single);
+    }
+}
